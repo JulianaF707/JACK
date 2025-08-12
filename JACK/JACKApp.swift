9e7864a3368535b783fd442dc7efd242144f723d
Juliana <julianafuchsnyc@gmail.com>
2025-08-12 09:31:36 -0400

    Initial Commit

---
+++ JACK/JACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  JACKApp.swift
+//  JACK
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct JACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
