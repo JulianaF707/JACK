f67cf5305af93a804aa7f5a47e091ce784f46b52
Charlotte <caw680@gmail.com>
2025-08-12 13:01:43 -0400

    Created Goals file, added heading

---
+++ Goals.swift
@@ -0,0 +1,22 @@
+//
+//  Goals.swift
+//  JACK
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+struct Goals: View {
+    var body: some View {
+        Text("Goals")
+            .font(.title)
+            .fontWeight(.bold)
+        Spacer()
+            .frame(height: 150)
+    }
+}
+
+#Preview {
+    Goals()
+}
